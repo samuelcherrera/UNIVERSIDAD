120c4f1112530f25fd091053d8ba001fdee05541
samuelcherrera <samuelcasta597gmail.com>
2025-04-21 20:16:18 -0500

    CRUD MATRICULA

---
+++ Classes/clsMatricula.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using UNIVERSIDAD.Models;
+
+namespace UNIVERSIDAD.Classes
+{
+    public class clsMatricula
+    {
+        private DBUniverdadEntities DBUniversidad = new DBUniverdadEntities();
+
+        public Matricula matricula { get; set; } //PARA MANIPULAR LA TABLA MATRICULA
+
+
+        public String Insertar()
+        {
+            try
+            {
+                DBUniversidad.Matriculas.Add(matricula); // agg una nueva matricula a la tabla MATRICULA (INSERT)
+                DBUniversidad.SaveChanges(); // guarda cambios en la bd
+                return "matricula ingresada correctamente " + matricula.Estudiante;
+            }
+            catch (Exception ex)
+            {
+                return "error al insertar la matricula " + ex.Message;
+            }
+
+        }
+
+
+      /* 
+        public Matricula ConsultarXDocumento(String Documento)
+        {
+            //EXPRESIONES LAMBDA:funciones anonimas que permiten filtrar los datos de una tabla
+            //FirstOrDefault: devuelve el primer elemento que cumpla con la condicion de la expresion lambda
+            Matricula mat = DBUniversidad.Matriculas.FirstOrDefault(e => e.Estudiante.Documento == Documento);//consulta la matricula por semestre de la matricula
+            return mat;
+        }
+
+        public Matricula ConsultarXSemestre(String Semestre)
+        {
+            //EXPRESIONES LAMBDA:funciones anonimas que permiten filtrar los datos de una tabla
+            //FirstOrDefault: devuelve el primer elemento que cumpla con la condicion de la expresion lambda
+            Matricula mat = DBUniversidad.Matriculas.FirstOrDefault(e => e.SemestreMatricula == Semestre);//consulta la matricula por documento del estudiante
+            return mat;
+        }
+      */
+        public Matricula ConsultarPorDocumentoYSemestre(string documento, string semestre)
+        {
+            // Utilizamos Where para filtrar las matrículas que cumplen AMBAS condiciones
+            Matricula mat = DBUniversidad.Matriculas
+                .Where(e => e.Estudiante.Documento == documento && e.SemestreMatricula == semestre)
+                .FirstOrDefault(); // Devolvemos la primera matrícula que coincida o null si no hay ninguna
+
+            return mat;
+        }
+
+        public String Actualizar()
+        {
+            //para corroborar que si se actualizo primero consultamos el empleado
+            Matricula mat = ConsultarPorDocumentoYSemestre(matricula.Estudiante.Documento, matricula.SemestreMatricula);
+            if (mat == null)
+            {
+                return "no existe matricula para el documento "+mat.Estudiante.Documento+" en el semestre: "+mat.SemestreMatricula;
+            }
+            DBUniversidad.Matriculas.AddOrUpdate(matricula);//actualiza el empleado de la tabla empleadoes
+            DBUniversidad.SaveChanges();
+            return "se ha actualizado la matricula correctamente";
+
+        }
+
+
+        public String Eliminar()
+        {
+            try
+            {
+                //consultamos el empleado
+                Matricula mat = ConsultarPorDocumentoYSemestre(matricula.Estudiante.Documento, matricula.SemestreMatricula);
+                if (mat == null)
+                {
+                    return "no existe matricula para el documento " + mat.Estudiante.Documento + " en el semestre: " + mat.SemestreMatricula;
+                }
+                DBUniversidad.Matriculas.Remove(matricula);//actualiza el empleado de la tabla empleadoes
+                DBUniversidad.SaveChanges();
+                return "se ha eliminado la matricula correctamente";
+
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;// MENSAJE DE ERROR
+            }
+        }
+    }
+}
---
+++ Controllers/MatriculaController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using UNIVERSIDAD.Classes;
+using UNIVERSIDAD.Models;   
+
+namespace UNIVERSIDAD.Controllers
+{
+    [RoutePrefix("api/Matriculas")]
+    [Authorize]
+    public class MatriculaController : ApiController
+    {
+
+        [HttpGet]
+        [Route("ConsultarPorDocumentoYSemestre")]
+        public Matricula ConsultarXDocumento(String Documento, String Semestre)
+        {
+            clsMatricula Matricula = new clsMatricula();
+            return Matricula.ConsultarPorDocumentoYSemestre(Documento, Semestre);
+        }
+
+        [HttpPost]
+        [Route("IngresarMatricula")]
+        public string Insertar([FromBody] Matricula matricula)
+        {
+            clsMatricula Matricula = new clsMatricula();
+
+            //SE LE ASIGNA EL OBJETO empleado AL OBJETO empleado DE LA CLASE clsEmpleado 
+            Matricula.matricula = matricula;
+
+            return Matricula.Insertar();
+        }
+
+        [HttpPut]
+        [Route("Actualizar")]
+        public string Actualizar([FromBody] Matricula matricula)
+        {
+            clsMatricula Matricula = new clsMatricula();
+            Matricula.matricula = matricula;
+
+            return Matricula.Actualizar();
+        }
+
+
+        [HttpDelete]
+        [Route("EliminarMatricula")]
+        public string Eliminar(string Documento, string Semestre)
+        {
+            clsMatricula Matricula = new clsMatricula();
+            return Matricula.Eliminar();
+        }
+    }
+}
